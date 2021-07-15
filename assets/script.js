c5cbf8a637ac26bfd4f5fc912d0840efb2eb172e
John-Mark Sackett <jmsackett91@gmail.com>
2021-07-14 20:00:11 -0400

    got the city name and current date to show up on the pagegit add .

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,55 @@
-var searchInput = document.getElementById("search-button");
-var cityInput = document.getElementById("city-input");
-var clearBtn = document.getElementById("clear-history");
-var cityName = document.getElementById("city-name");
-var currentPic = document.getElementById("current-pic");
-var currentTemp = document.getElementById("temperature");
-var currentHumidity = document.getElementById("humidity"); 4
-var currentWind = document.getElementById("wind-speed");
-var currentUV = document.getElementById("UV-index");
-var history = document.getElementById("history");
-var apiKey = "708187e40b564ddb4865c0cf80887413";
-let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+function initPage() {
+    var searchInput = document.getElementById("search-button");
+    var cityInput = document.getElementById("city-input");
+    var clearBtn = document.getElementById("clear-history");
+    var nameElement = document.getElementById("city-name");
+    var currentPic = document.getElementById("current-pic");
+    var currentTemp = document.getElementById("temperature");
+    var currentHumidity = document.getElementById("humidity"); 4
+    var currentWind = document.getElementById("wind-speed");
+    var currentUV = document.getElementById("UV-index");
+    var history = document.getElementById("history");
+    var apiKey = "708187e40b564ddb4865c0cf80887413";
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
 
 
-function getWeather(cityName) {
+    function getWeather(cityName) {
 
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-    fetch(queryURL)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-        })
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+        fetch(queryURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                var currentDate = new Date(data.dt * 1000);
+                var day = currentDate.getDate();
+                var month = currentDate.getMonth() + 1;
+                var year = currentDate.getFullYear();
+                nameElement.innerHTML = data.name + "(" + month + "/" + day + "/" + year + ")";
+            })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    searchInput.addEventListener("click", function () {
+        var searchTerm = cityInput.value;
+        getWeather(searchTerm);
+        searchHistory.push(searchTerm);
+        localStorage.setItem("search", JSON.stringify(searchHistory));
+    })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-searchInput.addEventListener("click", function () {
-    var searchTerm = cityInput.value;
-    getWeather(searchTerm);
-    searchHistory.push(searchTerm);
-    localStorage.setItem("search", JSON.stringify(searchHistory));
-})
+initPage()
